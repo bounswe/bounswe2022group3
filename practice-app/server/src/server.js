a3b92f732a892bf935c4c60b1744378e1cb5fb56
ersoykadir <eersooy61@gmail.com>
2022-05-12 23:38:40 +0300

    database connection established

--- practice-app/server/src/server.js
+++ practice-app/server/src/server.js
@@ -1,10 +1,40 @@
 const http = require("http");
+const mongoose = require('mongoose');
 require("dotenv").config();
 
 const app = require("./app");
 
 const PORT = process.env.API_PORT;
 const server = http.createServer(app);
+
+const DB = process.env.DB;
+
+const connectDB = async () => {
+    await mongoose
+        .connect(DB, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        .then(() => {
+            console.log("DB Connection established");
+            var connection = mongoose.connection;
+            connection.db.listCollections().toArray(function(err, names) {
+              if (err) {
+                  console.log(err);
+              }
+              else {
+                  names.forEach(function(e,i,a) {
+                      console.log("--->>", e.name);
+                  });
+              }
+            });
+        })
+        .catch((e) => {
+            console.log(e);
+        });
+
+};
+
 
 async function startServer() {
   server.listen(PORT, () => {
