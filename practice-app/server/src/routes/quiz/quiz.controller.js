b93053a3b102ed8f31cc866e28ee8708afa0498d
hsuleerkul <hatice.erkul@boun.edu.tr>
2022-05-15 21:25:20 +0300

    Create quiz completed
    Create quiz method now can send multiple requests. API response is also checked.

--- practice-app/server/src/routes/quiz/quiz.controller.js
+++ practice-app/server/src/routes/quiz/quiz.controller.js
@@ -1,5 +1,58 @@
-const axios = require("axios");
-const {Quiz, QuizCategories} = require("../../models/quiz/quiz.model");
+const axios = require("axios")
+
+const {Quiz, QuizCategories} = require("../../models/quiz/quiz.model")
+
+
+const quiz_url = 'https://opentdb.com/api.php'
+
+async function onequizrequest (rr) {
+
+    const input = 
+    {
+        amount: rr._questionCount,
+        category: rr._category,
+        difficulty: rr._difficulty,
+        type: rr._type,
+        encode: 'url3986'
+    }
+
+    const response = (await axios.post(quiz_url, null, {params:input})).data
+    var categoryno = rr._category
+    var count = rr._questionCount
+    
+    if(response.response_code == "0")
+    {
+        const results = response.results
+        let tmp = results
+        var questions = []
+
+        for (let j = 0; j< tmp.length; j++)
+        {
+            let x = decodeURIComponent(tmp[j].question)
+            let y = decodeURIComponent(tmp[j].correct_answer)
+            let z = decodeURI(tmp[j].incorrect_answers)
+
+            questions[j] = x + ", " + y + ", " + z 
+        }
+
+        return [questions, categoryno, count]
+    }
+    
+    else if(response.response_code == "1")
+    {
+        return [1, categoryno, count]
+    }
+
+    else if(response.response_code == "2")
+    {
+        return [2, categoryno, count]
+    }
+
+    else
+    {
+        return  [3, categoryno, count]
+    }
+}
 
 
 const QuizController = 
@@ -36,51 +89,56 @@ const QuizController =
 
     createQuiz: async function (req,res)
     {
-        const {_questionCount, _category, _difficulty, _type} = req.body;
+        const request = req.body.categories;
+        var user_quiz = []
+        var categories = []
+        var count = 0
 
-        const quiz_url = 'https://opentdb.com/api.php'
-
-        const input = 
+        for(let i = 0; i < request.length; i++)
         {
-            amount: _questionCount,
-            category: _category,
-            difficulty: _difficulty,
-            type: _type,
-            encode: 'url3986'
-        }
 
-        const response = (await axios.post(quiz_url, null, {params:input})).data
+            let retval = await onequizrequest(request[i])
+            var questions = retval[0]
+            var category = retval[1]
+            var qcount = retval[2]
 
-        if(response.response_code == "0")
-        {
-            const results = response.results
-            let tmp = results
-            const questions = []
-
-            for (let j = 0; j< tmp.length; j++)
+            if(questions == 1)
             {
-                let x = decodeURIComponent(tmp[j].question)
-                let y = decodeURIComponent(tmp[j].correct_answer)
-                let z = decodeURI(tmp[j].incorrect_answers)
+                return res.json("There are not enough questions")
+            }
+            else if(questions == 2)
+            {
+                return res.json("You have given an invalid input")
+            }
+            else if(questions == 3)
+            {
+                return res.status(404).json("Please try again")
+            }
+            else
+            {
+                categories.push(category)
+                user_quiz  = user_quiz.concat(questions)
+                count += qcount
+            }
+            
+            if(i == request.length -1){
 
-                questions[j] = x + ", " + y + ", " + z 
-                console.log("x, y, ve z : " + x+ y+ z);
+                const quiz = await Quiz.create
+                ({
+                    quiz_id : 9,
+                    questionCount : count,
+                    categories: categories,
+                    questions : user_quiz,
+                })
+                return res.status(200).json("Here is your quiz:\n" + quiz)
             }
 
-            const quiz = await Quiz.create
-            ({
-                quiz_id : 7,
-                questionCount : _questionCount,
-                categories: [_category],
-                questions : questions,
-
-            })
-            res.status(200).json({quiz})
         }
-        
-        
+        return res.status(500)
+
     }
+
 
 };
 
-module.exports = QuizController; 
+module.exports = QuizController
