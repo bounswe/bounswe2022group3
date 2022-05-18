d4d01dc75e5d6b25ea08b3e96150d95b9aa184d2
Salim Kemal Tirit <64011660+salimtirit@users.noreply.github.com>
2022-05-18 14:09:33 +0300

    file created routes for get and posts added

---
+++ practice-app/server/src/routes/lyrics/lyrics.route.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const { validate } = require("./lyrics.validate");
+const { handleValidation } = require("../../services/validate");
+const LyricsController = require("./lyrics.controller");
+
+const lyricsRouter = express.Router();
+
+lyricsRouter.get(
+    "/search_lyrics",
+    validate("search_lyrics"),
+    handleValidation,
+    LyricsController.searchLyrics
+);
+
+lyricsRouter.post(
+    "/save_lyrics",
+    validate("save_lyrics"),
+    handleValidation,
+    LyricsController.saveLyrics
+);
+
+lyricsRouter.get(
+    "/saved_lyrics",
+    validate("saved_lyrics"),
+    handleValidation,
+    LyricsController.savedLyrics
+);
+
+module.exports = lyricsRouter;
