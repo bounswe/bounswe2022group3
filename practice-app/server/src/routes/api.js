d67c49c216078a8fa3e33eae16d4b927a64b1f31
Salim Kemal Tirit <64011660+salimtirit@users.noreply.github.com>
2022-05-18 14:06:19 +0300

    /lyrics route added

--- practice-app/server/src/routes/api.js
+++ practice-app/server/src/routes/api.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const chessRouter = require("./chess/chess.route");
 const usersRouter = require("./users/users.route");
+const lyricsRouter = require("./lyrics/lyrics.route");
 
 const api = express.Router();
 
 api.use("/users", usersRouter);
 api.use("/chess", chessRouter);
+api.use("/lyrics", lyricsRouter);
 
 module.exports = {
   api,
