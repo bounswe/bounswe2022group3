f7449db33ed259b970dbe1d72c752cae9874639e
Brunettow <bengisutakkin34@hotmail.com>
2022-05-19 12:27:20 +0300

    Frontend completed

--- practice-app/client/pages/verify/verify.js
+++ practice-app/client/pages/verify/verify.js
@@ -1,3 +1,5 @@
+import { DataGrid } from "@mui/x-data-grid";
+import {Grid} from "@mui/material";
 import axios from "axios";
 import { Field, Form, Formik } from "formik";
 import Link from "next/link";
@@ -8,24 +10,31 @@ import Button from "../../components/Button/Button";
 import AuthLayout from "../../layouts/auth/AuthLayout";
 import { API_URL } from "../../next.config";
 import styles from "../../styles/user/form.module.scss";
+import { useEffect, useState } from "react"
 
 export default function verify() {
     const router = useRouter();
-
+    const [response_message, set_response_message] = useState(null)
+   // const [saved_emails, set_saved_emails] = useState([])
+    const [email, setEmail] = useState([]);
     const EmailSchema = Yup.object().shape({
         email: Yup.string()
-            .required("wow")
+            .required("")
     });
 
     const handleVerify = async (values) => {
-        console.log(values);
+        //console.log(values);
         try{
             const url = `${API_URL}/verify/verifyEmail`;
             const payload = {
                 'email': values.email
             };
             const response = (await axios.post(url, payload)).data;
-            console.log(response);
+            if(response.ok.data.deliverable)
+                set_response_message("This is a valid email.")
+            else
+                set_response_message("This email does not exist.")
+           // console.log(response);
       
         }catch(error){
             console.log(error);
@@ -39,27 +48,57 @@ export default function verify() {
                 'email': values.email
             };
             const response = (await axios.post(url, payload)).data;
-            console.log(response);
+            set_response_message("Email saved successfully.")
+            //console.log(response);
       
         }catch(error){
             console.log(error);
         }
     };
 
-    const handleGet = async (values) => {
+    /*const handleGet = async (values) => {
         try{
             const url = `${API_URL}/verify/getEmails`;
             const response = (await axios.get(url)).data;
+            set_saved_emails(response)
             console.log(response);
       
         }catch(error){
             console.log(error.toString());
         }
+    };*/
+
+    const emailColumns = [
+        {
+            field: "email",
+            headerName: "Saved Emails",
+            width: 200,
+        }
+    ];
+
+
+    const getEmails = async () => {
+        const url = `${API_URL}/verify/getEmails`;
+        const res = (await axios.get(url)).data;
+        //console.log(res)
+        if (res && res.emails) {
+            res.emails.forEach((email, k) => {
+                email.id = k;
+            });
+            setEmail(res.emails);
+        } else {
+            toast.warning("Something went wrong. Try again.");
+        }
     };
+
+    useEffect(() => {
+        getEmails();
+    }, []);
+
 
     return (
         <>
-            <h1></h1>
+            
             <Formik
                 initialValues={{
                     email: ""   
@@ -68,8 +107,7 @@ export default function verify() {
             >
                 {({ errors, touched, values }) => (
                     <Form className={styles.form}>
-                        <Button  onClick={handleGet}>View Emails</Button>
-                        
+
                         <Field
                             id="username"
                             name="email"
@@ -82,9 +120,32 @@ export default function verify() {
                                 {errors.email}
                             </div>
                         )}
+
+                        {
+                            response_message && (<div className={styles.error}>{response_message}</div>)
+                        }
+                        
                         <Button  onClick={() => handleVerify(values)}>Verify Email</Button>
                         <Button  onClick={() => handleSave(values)}>Save Email</Button>
                         
+                        <Grid container justifyContent="flex-end" sx={{ mb: 3 }}>
+                            {/*<Button
+                                variant="contained"
+                                onClick={() => router.push("/verify")}
+                            >
+                                Back
+                             </Button> */}
+                        </Grid>
+                        <Grid container>
+                            <DataGrid
+                                rows={email}
+                                columns={emailColumns}
+                                pageSize={5}
+                                checkboxSelection={false}
+                                disableSelectionOnClick
+                                autoHeight
+                            />
+                        </Grid>
                     </Form>
                 )}
             </Formik>
