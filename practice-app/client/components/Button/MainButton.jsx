28cd73fcaed65586b3b3bf841700f4bc33a79bea
nurlan2002 <nurlandadashov2002@yahoo.com>
2022-05-20 23:18:40 +0300

    buttons added

--- practice-app/client/components/Button/MainButton.jsx
+++ practice-app/client/components/Button/MainButton.jsx
@@ -1,11 +1,24 @@
-import styles from './Button.module.scss'
+import styles from "./Button.module.scss";
 
-function MainButton({type="button", style={},onClick=() => {}, children}) {
-  return (
-    <button type={type} style={style} onClick={onClick} className={styles.mainButton}>
-        {children}
-    </button>
-  )
+function MainButton({
+    color,
+    type = "button",
+    style = {},
+    onClick = () => {},
+    children,
+}) {
+    return (
+        <button
+            type={type}
+            style={style}
+            onClick={onClick}
+            className={`${styles.mainButton} ${
+                color === "red" ? styles.mainButton_red : styles.mainButton_blue
+            }`}
+        >
+            {children}
+        </button>
+    );
 }
 
-export default MainButton
+export default MainButton;
--- practice-app/client/pages/user/main.js
+++ practice-app/client/pages/user/main.js
@@ -8,10 +8,35 @@ export default function main() {
     const router = useRouter();
     const [user_name, setUsername] = useState("");
 
-    const handleClick = async (values) => {
-        router.push(`/chess`);
-    };
-// 
+    const handleClick = async (route) => {
+        if (route === "chess") {
+            router.push('/chess');
+        }
+        else if (route === "quiz") {
+            router.push('/quiz');
+        }
+        else if (route === "movies") {
+            router.push('/movies/search');
+        }
+        else if (route === "coin") {
+            router.push('/coin');
+        }
+        else if (route === "tweets") {
+            router.push('/tweet');
+        }
+        else if (route === "lyrics") {
+            router.push('/lyrics');
+        }
+        else if (route === "posts") {
+            router.push('/posts');
+        }
+        else if (route === "currency") {
+            router.push('/currency');
+        }
+        else if (route === "verify") {
+            router.push('/verify/verify');
+        }
+    }; 
 
     const getUsername = async () => {
         const email = localStorage.getItem("email");
@@ -34,19 +59,24 @@ export default function main() {
     return (
         <>
             <h1 style={{marginBottom:"0"}}>Welcome to The Desert of the Real !</h1>
-                <h2 style={{marginBottom:"20px"}}>{user_name}</h2>
-                <h2 style={{marginBottom:"30px"}}>Choose Wisely!</h2>
-            {/* <Image src="/desert.png"  width="729" height="300" /> */}
+            <h2 style={{marginBottom:"20px"}}>{user_name}</h2>
+            <h2 style={{marginBottom:"30px"}}>Choose Wisely!</h2>
+
             <div className="grid-container">
                 <div className="grid-item" style={{display:"inline-block"}}>
-                <MainButton style={{backgroundColor:"#4253b5"}} onClick={handleClick}>Chess</MainButton>
-                <MainButton style={{backgroundColor:"#4253b5"}}>Login</MainButton>
-                <MainButton style={{backgroundColor:"#4253b5"}}>Login</MainButton>
+                    <MainButton color="blue" onClick={() => handleClick("chess")}>Chess</MainButton>
+                    <MainButton color="blue" onClick={() => handleClick("quiz")}>Quiz</MainButton>
+                    <MainButton color="blue" onClick={() => handleClick("movies")}>Movies</MainButton>
+                    <MainButton color="blue" onClick={() => handleClick("coin")}>Coin</MainButton>
                 </div>
                 <div className="grid-item" style={{display:"inline-block"}}>
-                <MainButton style={{backgroundColor:"#CB3C27"}}>Login</MainButton>
-                <MainButton style={{backgroundColor:"#CB3C27"}}>Login</MainButton>
-                <MainButton style={{backgroundColor:"#CB3C27"}}>Login</MainButton>
+                    <MainButton color="red" onClick={() => handleClick("tweets")}>Tweets</MainButton>
+                    <MainButton color="red" onClick={() => handleClick("lyrics")}>Lyrics</MainButton>
+                    <MainButton color="red" onClick={() => handleClick("currency")}>Currency</MainButton>
+                    <MainButton color="red" onClick={() => handleClick("posts")}>Posts</MainButton>
+                </div>
+                <div className="grid-item" style={{display:"inline-block"}}>
+                    <MainButton color="red" onClick={() => handleClick("verify")}>Verify</MainButton>
                 </div>
             </div>
         </>
