8bb63616c5f77f54eff0b963710875e114c7a11e
Muhammet Şen <enmuhammet23@gmail.com>
2022-10-23 16:10:09 +0300

    mobile: add search functionality to homepage

--- app/mobile/lib/services/course_service.dart
+++ app/mobile/lib/services/course_service.dart
@@ -10,16 +10,19 @@ class Course {
   Course(this.title, this.body, {this.image, this.rating, this.teacher});
 }
 
+List<Course> mockCourses = [
+  for (var i = 0; i < 100; ++i)
+    Course('Basics of Google UI/UX Designs - ${i.toString()}',
+        "Interested in learning more about data science, but don’t know where to start?" * 10,
+        image: 'https://cdn-icons-png.flaticon.com/512/2991/2991148.png', teacher: "Andrew mg", rating: 4.3)
+];
+
 @lazySingleton
 class CourseService {
-  List<Course> courses() {
-    return [
-      for (var i = 0; i < 100; ++i)
-        Course("Introduction to Data Science with Python",
-            "Interested in learning more about data science, but don’t know where to start?" * 10,
-            image: 'https://cdn.educba.com/academy/wp-content/uploads/2019/03/Introduction-To-Data-Science.jpg.webp',
-            teacher: "Andrew mg",
-            rating: 4.3)
-    ];
+  courses() => mockCourses;
+
+  Future<List<Course>> searchCourse(String keyword) async {
+    await Future.delayed(const Duration(milliseconds: 300));
+    return mockCourses.where((element) => (element.title.contains(keyword) || element.body.contains(keyword))).toList();
   }
 }
