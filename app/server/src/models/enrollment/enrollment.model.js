052a01db637516bc654082ee8d01a2f1dada99f9
Furkan Akkurt <71407287+furkanakkurt1335@users.noreply.github.com>
2022-10-23 19:09:06 +0300

    change created_at field from enrollment model to timestamps

--- app/server/src/models/enrollment/enrollment.model.js
+++ app/server/src/models/enrollment/enrollment.model.js
@@ -1,34 +1,33 @@
 const mongoose = require("mongoose");
 
-const enrollmentSchema = new mongoose.Schema({
-  user_id: {
-    type: String,
+const enrollmentSchema = new mongoose.Schema(
+  {
+    user_id: {
+      type: String,
+    },
+    course: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+    },
+    is_active: {
+      type: Boolean,
+    },
+    notes: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "Note",
+    },
+    progress: {
+      type: Map,
+      of: Boolean,
+    },
   },
-  course: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Course",
-  },
-  created_at: {
-    type: Date,
-  },
-  is_active: {
-    type: Boolean,
-  },
-  notes: {
-    type: [mongoose.Schema.Types.ObjectId],
-    ref: "Note",
-  },
-  progress: {
-    type: Map,
-    of: Boolean,
-  },
-});
+  { timestamps: true }
+);
 
 const Enrollment = mongoose.model("Enrollment", enrollmentSchema);
 
 const createEnrollment = async (user_id, course_id) => {
   var enrollment = new Enrollment({ user_id, course_id });
-  enrollment.created_at = Date.now();
   const res = await enrollment.save();
   return res;
 };
