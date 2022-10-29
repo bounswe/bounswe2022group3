fa3b09c739b1b82cd40b972ae72ca26ea57f8fa7
Muhammet Şen <enmuhammet23@gmail.com>
2022-10-29 20:28:23 +0300

    mobile: fix overflow bug in SilverAppBar

--- app/mobile/lib/view/course/coursepage.dart
+++ app/mobile/lib/view/course/coursepage.dart
@@ -64,7 +64,7 @@ Widget coursePageView(Course c) =>
               borderRadius:
                   BorderRadius.all(Radius.circular(Constants.borderRadius)),
             ),
-            child: Text(c.info, maxLines: 3),
+            child: Text(c.info, maxLines: 5),
             // child:Text('Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',maxLines: 4,)
           );
 
@@ -79,6 +79,7 @@ Widget coursePageView(Course c) =>
                       title:  Text(c.name),
                       pinned: true,
                       flexibleSpace: FlexibleSpaceBar(
+                        collapseMode: CollapseMode.pin,
                         background: Container(
                           padding: const EdgeInsets.symmetric(horizontal: 10),
                           decoration: const BoxDecoration(
