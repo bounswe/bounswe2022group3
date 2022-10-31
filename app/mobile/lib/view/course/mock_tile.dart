bfb5c2cabc66035a76a7e87d002a9137a8ca2234
Salim Kemal Tirit <64011660+salimtirit@users.noreply.github.com>
2022-11-01 00:46:42 +0300

    mock tile is the content tile with mock data

---
+++ app/mobile/lib/view/course/mock_tile.dart
@@ -0,0 +1,17 @@
+import 'package:bucademy/classes/chapter/chapter.dart';
+import 'package:bucademy/resources/constants.dart';
+import 'package:bucademy/resources/custom_colors.dart';
+import 'package:bucademy/services/content_service.dart';
+import 'package:flutter/material.dart';
+
+Widget mockTile(MockContent c) {
+  return Container(
+    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+    margin: const EdgeInsets.only(bottom: 10),
+    decoration: BoxDecoration(
+      color: CustomColors.getRandomColor(),
+      borderRadius: BorderRadius.circular(Constants.borderRadius),
+    ),
+    child: Text(c.name),
+  );
+}
