26dcdb29de18710e3142557b65eec11469dbb9fb
Furkan Akkurt <71407287+furkanakkurt1335@users.noreply.github.com>
2022-11-25 23:58:58 +0300

    add timestamps to resource model

--- app/server/src/models/resource/resource.model.js
+++ app/server/src/models/resource/resource.model.js
@@ -1,21 +1,25 @@
 const mongoose = require("mongoose");
 
-const resourceSchema = new mongoose.Schema({
-  name: {
-    type: String,
+const resourceSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+    },
+    body: {
+      type: String,
+    },
+    topic: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Topic",
+    },
+    discussion: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Discussion",
+    },
   },
-  body: {
-    type: String,
-  },
-  topic: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Topic",
-  },
-  discussion: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Discussion",
-  },
-});
+  { timestamps: true }
+);
+
 const Resource = mongoose.model("Resource", resourceSchema);
 
 const createResource = async (name, body, topic) => {
