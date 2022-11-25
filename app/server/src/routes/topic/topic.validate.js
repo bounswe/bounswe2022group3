5e1373378a9d7add82ad17b726565ab304723db3
Furkan Akkurt <71407287+furkanakkurt1335@users.noreply.github.com>
2022-11-26 00:15:46 +0300

    fix create-topic validate

--- app/server/src/routes/topic/topic.validate.js
+++ app/server/src/routes/topic/topic.validate.js
@@ -1,12 +1,11 @@
-const { body, query } = require("express-validator");
+const { body } = require("express-validator");
 
 exports.validate = (method) => {
   switch (method) {
     case "create-topic":
       return [
         body("name", "name doesn't exist").exists(),
-        body("badge", "badge doesn't exist").exists(),
-        body("resources", "resources don't exist").exists().isArray(),
+        body("space_id", "space_id doesn't exist").exists(),
       ];
   }
 };
