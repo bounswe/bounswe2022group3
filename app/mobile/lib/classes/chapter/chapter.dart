9d1ad48dde4b8a145a531cb19e17f3da492700da
Muhammet Şen <enmuhammet23@gmail.com>
2022-11-27 20:41:13 +0300

    Create resource class

--- app/mobile/lib/classes/chapter/chapter.dart
+++ app/mobile/lib/classes/chapter/chapter.dart
@@ -1,19 +1,20 @@
+import 'package:bucademy/classes/resource/resource.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'chapter.g.dart';
 
 @JsonSerializable()
 class Chapter {
-  @JsonKey(name: 'chapter_name')
+  @JsonKey(name: 'name')
   final String name;
 
   @JsonKey(name: '_id')
   final String id;
 
-  @JsonKey(name: 'content')
-  final List<String> cwontentIds;
+  @JsonKey(name: 'resources')
+  final List<Resource> resources;
 
-  Chapter(this.name, this.id, this.cwontentIds);
+  Chapter(this.name, this.id, this.resources);
   factory Chapter.fromJson(Map<String, dynamic> json) => _$ChapterFromJson(json);
 
   Map<String, dynamic> toJson() => _$ChapterToJson(this);
