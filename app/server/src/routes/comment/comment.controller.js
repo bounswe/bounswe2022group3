ed8981e043e85d36722b3b53bdc8eeba2b0e3dd8
Furkan Akkurt <71407287+furkanakkurt1335@users.noreply.github.com>
2022-11-27 15:17:29 +0300

    update create comment function of comment controller to include comment in a discussion object if discussion_id is given

--- app/server/src/routes/comment/comment.controller.js
+++ app/server/src/routes/comment/comment.controller.js
@@ -1,11 +1,19 @@
 const CommentModel = require("../../models/comment/comment.model");
+const DiscussionModel = require("../../models/discussion/discussion.model");
 
 const CommentController = {
   createComment: async function (req, res) {
     try {
-      const { user, body, files } = req.body;
-
+      const { body, files } = req.body;
+      const user = req.auth.id;
       const comment = await CommentModel.createComment(user, body, files);
+      var body_keys = Object.keys(req.body);
+      if (body_keys.includes("discussion_id")) {
+        const { discussion_id } = req.body;
+        var discussion = DiscussionModel.Discussion.findById(discussion_id);
+        discussion.comments.push(comment);
+        discussion.save();
+      }
       res.status(201).json({ message: comment });
     } catch (e) {
       console.log("Error on getCreate:", e);
--- app/server/src/routes/comment/comment.validate.js
+++ app/server/src/routes/comment/comment.validate.js
@@ -4,7 +4,6 @@ exports.validate = (method) => {
   switch (method) {
     case "create-comment":
       return [
-        body("user", "user doesn't exist").exists(),
         body("body", "body doesn't exist").exists(),
         body("files", "files don't exist").exists().isArray(),
       ];
