7916392c9acc8e1429055fb7059c7db9e9a6a150
ersoykadir <eersooy61@gmail.com>
2022-11-27 21:39:12 +0300

    backend: getEnrolledSpaces done

--- app/server/src/routes/enrollment/enrollment.controller.js
+++ app/server/src/routes/enrollment/enrollment.controller.js
@@ -8,7 +8,7 @@ const EnrollmentController = {
       const space_id = req.body.space_id;
       const user = req.auth.id;
       const space = await SpaceModel.Space.findById(space_id);
-      if(!space){
+      if (!space) {
         return res.status(400).json({ error: "Space does not exist!" });
       }
       const enrolled_space = await EnrollmentModel.Enrollment.find({
@@ -16,11 +16,11 @@ const EnrollmentController = {
         space: space_id,
       });
       console.log(enrolled_space)
-      if(enrolled_space.length > 0){
+      if (enrolled_space.length > 0) {
         return res.status(400).json({ error: "User already enrolled!" });
       }
       const enrollment = await EnrollmentModel.createEnrollment(user, space_id);
-      space.enrollments.push(enrollment); 
+      space.enrollments.push(enrollment);
       space.save();
       return res.status(201).send({ enrollment });
     } catch (e) {
@@ -68,25 +68,47 @@ const EnrollmentController = {
 
   getEnrolledSpaces: async function (req, res) {
     try {
+      const keyword = req.params.keyword;
       const user = req.auth.id;
-      const enrolled_spaces = await EnrollmentModel.Enrollment.find({
-        user,
-      });
-      var spaces = [];
-      for (var enrolled_space of enrolled_spaces) {
-        var space = await SpaceModel.Space.findById(enrolled_space.space)
+      var enrollments = [];
+      if (keyword) {
+        let spaces = await SpaceModel.Space.find(
+          {
+            name: { $regex: keyword, $options: "i" },
+          },
+          "name creator info rating tags image"
+        )
           .populate("creator", "name surname")
-          .populate({
-            path: "topics",
-            populate: {
-              path: "badge",
+          .exec();
+        for (var space of spaces) {
+          var enr = await EnrollmentModel.Enrollment.find(
+            {
+              space,
+              user,
             },
-          });
-        if (space) {
-          spaces.push(space);
+            "space is_active notes progress"
+          ).exec();
+          if (enr) {
+            enrollments.push(space);
+          }
         }
       }
-      return res.status(200).json({ spaces });
+      else {
+        const enrolled_spaces = await EnrollmentModel.Enrollment.find({
+          user,
+        });
+        for (var enrolled_space of enrolled_spaces) {
+          var space = await SpaceModel.Space.find({
+            _id: enrolled_space.space
+          },
+          "name creator info rating tags image"
+          ).populate("creator", "name surname");
+          if (space) {
+            enrollments.push(space[0]);
+          }
+        }
+      }
+      return res.status(200).json({ enrollments });
     } catch (e) {
       return res.status(400).send({ error: e.toString() });
     }
