bd9b335330b05f596564d14a3e40217fac55b421
Muhammet Şen <enmuhammet23@gmail.com>
2022-12-02 22:54:55 +0300

    Create NodeService

---
+++ app/mobile/lib/services/note_service.dart
@@ -0,0 +1,11 @@
+import 'package:bucademy/classes/discussion/comment.dart';
+import 'package:bucademy/classes/discussion/discussion.dart';
+import 'package:bucademy/services/locator.dart';
+import 'package:dio/dio.dart';
+import 'package:injectable/injectable.dart';
+
+@lazySingleton
+class NoteService {
+  
+
+}
