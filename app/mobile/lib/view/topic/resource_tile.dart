530c659c75119ce682611f74a3f5f2ebec952407
Salim Kemal Tirit <64011660+salimtirit@users.noreply.github.com>
2022-12-04 15:04:28 +0300

    delete tile added and the order changed

--- app/mobile/lib/view/topic/resource_tile.dart
+++ app/mobile/lib/view/topic/resource_tile.dart
@@ -9,7 +9,8 @@ import 'package:url_launcher/url_launcher.dart';
 Widget resourceTile(Resource r, BuildContext context) {
   return Container(
       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-      margin: const EdgeInsets.only(bottom: 10)+EdgeInsets.symmetric(horizontal: 10),
+      margin: const EdgeInsets.only(bottom: 10) +
+          EdgeInsets.symmetric(horizontal: 10),
       decoration: BoxDecoration(
         color: Color.fromARGB(255, 242, 241, 248),
         borderRadius: BorderRadius.circular(Constants.borderRadius),
@@ -19,20 +20,55 @@ Widget resourceTile(Resource r, BuildContext context) {
           showDialog(
               context: context,
               builder: ((context) => AlertDialog(
-                    content: Column(children: [
-                      Container(
-                        child: Text("Edit"),
-                        color: Colors.blue[200],
-                      ),
-                      Container(
-                        child: Text("Details"),
-                        color: Colors.blue[100],
-                      ),
-                      Container(
-                        child: Text("Discussion"),
-                        color: Colors.blue[200],
-                      ),
-                    ]),
+                    content: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Container(
+                            width: double.infinity,
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 20),
+                            child: Text(
+                              "Discussion",
+                              style: TextStyle(color: Colors.white),
+                              textAlign: TextAlign.center,
+                            ),
+                            color: CustomColors.main,
+                          ),
+                          Container(
+                            width: double.infinity,
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 20),
+                            child: Text(
+                              "Details",
+                              style: TextStyle(color: Colors.white),
+                              textAlign: TextAlign.center,
+                            ),
+                            color: Color.fromARGB(255, 68, 84, 141),
+                          ),
+                          Container(
+                            width: double.infinity,
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 20),
+                            child: Text(
+                              "Edit",
+                              style: TextStyle(color: Colors.white),
+                              textAlign: TextAlign.center,
+                            ),
+                            color: CustomColors.main,
+                          ),
+                          Container(
+                            width: double.infinity,
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 20),
+                            child: Text(
+                              "Delete",
+                              style: TextStyle(color: Colors.white),
+                              textAlign: TextAlign.center,
+                            ),
+                            color: Color.fromARGB(255, 68, 84, 141),
+                          ),
+                        ]),
                   )));
         },
         child: Column(
