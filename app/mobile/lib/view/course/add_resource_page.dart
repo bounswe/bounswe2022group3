45e2295137174bfd7d07e5c7e04e1a96d33411f2
Salim Kemal Tirit <64011660+salimtirit@users.noreply.github.com>
2022-12-04 11:53:28 +0300

    markdown input and other changes

--- app/mobile/lib/view/course/add_resource_page.dart
+++ app/mobile/lib/view/course/add_resource_page.dart
@@ -1,16 +1,15 @@
-import 'package:bucademy/classes/course/course.dart';
+import 'package:bucademy/classes/resource/resource.dart';
 import 'package:bucademy/classes/topic/topic.dart';
 import 'package:bucademy/resources/custom_colors.dart';
 import 'package:bucademy/resources/constants.dart';
 import 'package:bucademy/resources/text_styles.dart';
 import 'package:bucademy/services/locator.dart';
-import 'package:bucademy/view/home/appbar.dart';
-import 'package:bucademy/view/home/search_bar.dart';
 import 'package:bucademy/view/widgets/markdown_input.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 
-Widget addContentView(Topic c) => ViewModelBuilder<HomeViewModel>.reactive(
+Widget addContentView(TopicDetailed c) =>
+    ViewModelBuilder<HomeViewModel>.reactive(
       viewModelBuilder: () => HomeViewModel(),
       builder: (context, viewModel, child) => Scaffold(
           appBar: AppBar(
@@ -27,67 +26,108 @@ Widget addContentView(Topic c) => ViewModelBuilder<HomeViewModel>.reactive(
               ),
             ),
           ),
-          body: Form(
-            key: viewModel._formKey,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                      enabledBorder: const OutlineInputBorder(
-                          borderSide:
-                              const BorderSide(color: CustomColors.main)),
-                      border: OutlineInputBorder(),
-                      labelText: 'Enter Resource Name',
+          body: SingleChildScrollView(
+            child: Form(
+              key: viewModel._formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                    child: TextFormField(
+                      controller: viewModel.controller_title,
+                      decoration: const InputDecoration(
+                        enabledBorder: const OutlineInputBorder(
+                            borderSide:
+                                const BorderSide(color: CustomColors.main)),
+                        border: OutlineInputBorder(),
+                        labelText: 'Enter Resource Name',
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please Enter Some Text';
+                        }
+                        return null;
+                      },
                     ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please Enter Some Text';
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Container(
+                          child: Text(
+                        "Enter Resource Body:",
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      )),
+                    ),
+                  ),
+                  viewModel.isLoading
+                      ? loadingIndicator()
+                      : Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 8),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: const Color.fromARGB(255, 242, 241, 248),
+                              borderRadius:
+                                  BorderRadiusDirectional.circular(10),
+                              border: Border.all(color: CustomColors.main),
+                            ),
+                            child: markdownInput(null, viewModel.controller,
+                                loading: viewModel.isLoading,
+                                maxLines: 20,
+                                sendText: '',
+                                sendIcon: Icons.abc,
+                                withBorderRadius: true),
+                          ),
+                        ),
+                  GestureDetector(
+                    onTap: () async {
+                      if (viewModel._formKey.currentState!.validate()) {
+                        Resource? created =
+                            await contentService.createNewResource(
+                                topicId: c.id,
+                                name: viewModel.controller_title.text,
+                                body: viewModel.controller.text);
+                        if (created!.id.isNotEmpty) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                                content: Text('Successfully Created!')),
+                          );
+                          viewModel.addNewResource(c, created);
+                          Navigator.pop(context);
+                          viewModel.notifyListeners();
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                                content: Text('Could Not Create Resource!')),
+                          );
+                          viewModel.notifyListeners();
+                        }
                       }
-                      return null;
                     },
-                  ),
-                ),
-                //markdownInput(viewModel, controller),
-                GestureDetector(
-                  onTap: () async {
-                    if (viewModel._formKey.currentState!.validate()) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text('Processing Data')),
-                      );
-                    }
-                    viewModel.notifyListeners();
-                  },
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(vertical: 8),
-                    margin: const EdgeInsets.symmetric(vertical: 8),
-                    width: 120,
-                    decoration: BoxDecoration(
-                      borderRadius:
-                          BorderRadius.circular(Constants.borderRadius),
-                      color: CustomColors.main,
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(vertical: 8),
+                      margin: const EdgeInsets.symmetric(vertical: 8),
+                      width: 120,
+                      decoration: BoxDecoration(
+                        borderRadius:
+                            BorderRadius.circular(Constants.borderRadius),
+                        color: CustomColors.main,
+                      ),
+                      child: Center(
+                          child: Text(
+                        "Create",
+                        style: TextStyles.bodyWhite,
+                      )),
                     ),
-                    child: Center(
-                        child: Text(
-                      "Create",
-                      style: TextStyles.bodyWhite,
-                    )),
                   ),
-                ),
-                // ElevatedButton(
-                //   onPressed: () {
-                //     if (viewModel._formKey.currentState!.validate()) {
-                //       ScaffoldMessenger.of(context).showSnackBar(
-                //         const SnackBar(content: Text('Processing Data')),
-                //       );
-                //     }
-                //   },
-                //   child: const Text('Submit'),
-                // ),
-              ],
+                ],
+              ),
             ),
           )),
     );
@@ -95,27 +135,18 @@ Widget addContentView(Topic c) => ViewModelBuilder<HomeViewModel>.reactive(
 // ViewModel
 class HomeViewModel extends ChangeNotifier {
   String? title;
+  String? body;
   bool isLoading = false;
-  bool isSearchScreen = false;
 
   final _formKey = GlobalKey<FormState>();
 
-  Future<void> search(String keyword) async {
-    bool newState = keyword.isNotEmpty;
-    if (newState != isSearchScreen) {
-      isSearchScreen = newState;
-      notifyListeners();
-    }
+  TextEditingController controller = TextEditingController();
+  TextEditingController controller_title = TextEditingController();
 
-    isLoading = true;
-    notifyListeners();
-
-    // searchResults = await contentService.createNewTopic();
-    isLoading = false;
+  void addNewResource(TopicDetailed t, Resource r) {
+    t.resources.insert(0, r);
     notifyListeners();
   }
-
-  
 }
 
 Widget loadingIndicator() => const Padding(
