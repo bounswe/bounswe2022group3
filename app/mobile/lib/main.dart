15e1d4b3fb610b81818260a04039004d686816af
Muhammet Şen <enmuhammet23@gmail.com>
2022-12-05 23:52:31 +0300

    Add http override for android certificates

--- app/mobile/lib/main.dart
+++ app/mobile/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:bucademy/resources/custom_colors.dart';
 import 'package:bucademy/services/locator.dart';
 import 'package:bucademy/view/intro/intro.dart';
@@ -5,7 +6,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
+
 void main() {
+  HttpOverrides.global = MyHttpOverrides();
   configureDependencies();
   navigatorService.controller = PersistentTabController(initialIndex: 0);
   WidgetsFlutterBinding.ensureInitialized();
