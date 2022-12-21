a8d66674e27c57f989ac34d30b9b97001f1622cb
Muhammet Şen <enmuhammet23@gmail.com>
2022-12-21 11:59:48 +0300

    mobile - close the keyboard and clear the text on go back

--- app/mobile/lib/view/home/search_bar.dart
+++ app/mobile/lib/view/home/search_bar.dart
@@ -2,13 +2,23 @@ import 'package:bucademy/resources/custom_colors.dart';
 import 'package:bucademy/resources/text_styles.dart';
 import 'package:flutter/material.dart';
 
-TextFormField searchBar(void Function(String)? onChanged,) {
+TextFormField searchBar(
+    void Function(String)? onChanged, TextEditingController controller,
+    {close = Function}) {
   return TextFormField(
     cursorColor: CustomColors.main,
+    textAlignVertical: TextAlignVertical.center,
+    controller: controller,
     maxLines: 1,
     onChanged: onChanged,
     decoration: InputDecoration(
-      border: OutlineInputBorder(borderRadius: BorderRadius.circular(10.0), borderSide: BorderSide.none),
+      prefix: GestureDetector(
+        onTap: close,
+        child: Icon(Icons.arrow_back, color: Colors.grey.shade500, size: 20),
+      ),
+      border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10.0),
+          borderSide: BorderSide.none),
       hintText: 'Search Courses...',
       hintStyle: TextStyles.helperText,
       fillColor: Colors.white,
