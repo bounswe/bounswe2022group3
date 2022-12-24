b8ebd61e0ff9c1fb062f981d1dce7b88b47d79af
bilozof <bilal.aytekin@boun.edu.tr>
2022-12-24 13:45:52 +0300

    end date and quota optional, date checks

--- app/mobile/lib/view/course/event/create_event.dart
+++ app/mobile/lib/view/course/event/create_event.dart
@@ -26,15 +26,16 @@ createEvent(BuildContext context, CoursePageViewModel viewModel) {
       builder: ((context) => AlertDialog(
             title: const Text('Enter the details of the event'),
             content: Column(
+              mainAxisSize: MainAxisSize.min,
               children: [
                 TextField(
                   onChanged: (value) => title = value,
-                  decoration: const InputDecoration(hintText: "Title"),
+                  decoration: const InputDecoration(hintText: "Title*"),
                 ),
                 DateTimeField(
                     format: format,
                     decoration: const InputDecoration(
-                        hintText: 'Set the starting date'),
+                        hintText: 'Set the starting date*'),
                     onShowPicker: (context, currentValue) async {
                       final date = await showDatePicker(
                           context: context,
@@ -57,8 +58,8 @@ createEvent(BuildContext context, CoursePageViewModel viewModel) {
                     }),
                 DateTimeField(
                     format: format,
-                    decoration: const InputDecoration(
-                        hintText: 'Set the starting date'),
+                    decoration:
+                        const InputDecoration(hintText: 'Set the ending date'),
                     onShowPicker: (context, currentValue) async {
                       final date = await showDatePicker(
                           context: context,
@@ -83,10 +84,12 @@ createEvent(BuildContext context, CoursePageViewModel viewModel) {
                     keyboardType: TextInputType.number,
                     onChanged: (value) => quota = int.parse(value),
                     decoration: const InputDecoration(hintText: "Quota")),
-                TextField(
-                  onChanged: (value) => description = value,
-                  decoration: const InputDecoration(hintText: "Description"),
-                  maxLines: 15,
+                Flexible(
+                  child: TextField(
+                    onChanged: (value) => description = value,
+                    decoration: const InputDecoration(hintText: "Description*"),
+                    maxLines: 15,
+                  ),
                 ),
                 /*MapLocationPicker(
                         apiKey: 'AIzaSyAXTzbniq2PnBkL1GbxGzCqv4rFCgAaFpA',
@@ -115,39 +118,56 @@ createEvent(BuildContext context, CoursePageViewModel viewModel) {
                 ),
               ),
               GestureDetector(
-                onTap: (title != null &&
-                        title!.isNotEmpty &&
-                        startDate != null &&
-                        endDate != null &&
-                        description != null &&
-                        description!.isNotEmpty &&
-                        quota != null
-                    //&& visibility != null &&
-                    //visibility!.isNotEmpty &&
-                    //fee != null &&
-                    //medium != null &&
-                    //medium!.isNotEmpty &&
-                    )
-                    ? null
-                    : () async {
-                        Event? e = await eventService.createEvent(
-                            spaceId: viewModel.course!.id,
-                            creator: userService.user!.id,
-                            title: title!,
-                            startDate: startDate!.toIso8601String(),
-                            endDate: endDate!.toIso8601String(),
-                            location: location!,
-                            description: description!,
-                            quota: quota!);
-                        //visibility: visibility!,
-                        //fee: fee!,
-                        //medium: medium!,
-                        if (e != null) {
-                          viewModel.addNewEvent(e);
-                          print('Here!');
-                        }
-                        Navigator.pop(context);
-                      },
+                onTap: () async {
+                  if (title != null &&
+                      title!.isNotEmpty &&
+                      startDate != null &&
+                      description != null &&
+                      description!.isNotEmpty) {
+                    if (startDate!.compareTo(DateTime.now()) == -1) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                            content: Text('Start date cannot be in the past.')),
+                      );
+                    } else if (endDate != null &&
+                        startDate!.compareTo(endDate!) == 1) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                            content: Text(
+                                'Start date cannot be after the end date.')),
+                      );
+                    } else if (endDate != null &&
+                        startDate!.compareTo(endDate!) == 1) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                            content:
+                                Text('End date cannot be later than a year.')),
+                      );
+                    } else {
+                      Event? e = await eventService.createEvent(
+                          spaceId: viewModel.course!.id,
+                          creator: userService.user!.id,
+                          title: title!,
+                          startDate: startDate!.toIso8601String(),
+                          endDate: endDate == null
+                              ? null
+                              : endDate!.toIso8601String(),
+                          location: location!,
+                          description: description!,
+                          quota: quota);
+                      if (e != null) {
+                        viewModel.addNewEvent(e);
+                      }
+                      Navigator.pop(context);
+                    }
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                          content:
+                              Text('You should enter all necessary info.')),
+                    );
+                  }
+                },
                 child: Container(
                   padding:
                       const EdgeInsets.symmetric(vertical: 6, horizontal: 8),
