4c3544ae8208aa4e0cb0887f221a8e29e93bd1e4
Salim Kemal Tirit <64011660+salimtirit@users.noreply.github.com>
2022-12-25 02:38:57 +0300

    maps launcher added

--- app/mobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ app/mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,13 @@ import FlutterMacOS
 import Foundation
 
 import geolocator_apple
+import maps_launcher
 import shared_preferences_macos
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  MapsLauncherPlugin.register(with: registry.registrar(forPlugin: "MapsLauncherPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
--- app/mobile/windows/flutter/generated_plugins.cmake
+++ app/mobile/windows/flutter/generated_plugins.cmake
@@ -4,6 +4,7 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   geolocator_windows
+  maps_launcher
   url_launcher_windows
 )
 
