1d0f326fa2c4f80a80dcc68207b5aa2e2510b44a
Muhammet Şen <enmuhammet23@gmail.com>
2022-12-25 22:08:06 +0300

    mobile - open note view directly from deep links

--- app/mobile/lib/view/intro/intro.dart
+++ app/mobile/lib/view/intro/intro.dart
@@ -1,9 +1,12 @@
+import 'package:bucademy/classes/note/note.dart';
 import 'package:bucademy/resources/constants.dart';
 import 'package:bucademy/resources/custom_colors.dart';
 import 'package:bucademy/resources/text_styles.dart';
 import 'package:bucademy/services/locator.dart';
+import 'package:bucademy/view/course/note/note_view.dart';
 import 'package:bucademy/view/dashboard_view.dart';
 import 'package:flutter/material.dart';
+import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 import 'package:stacked/stacked.dart';
 import 'package:uni_links/uni_links.dart';
 import '../login/login.dart';
@@ -39,27 +42,28 @@ class IntroViewModel extends ChangeNotifier {
     }
     // Get the initial link
     Uri? initialLink = await getInitialUri();
-    await handleInitialLink(initialLink);
-    await Future.wait([
-      userService.refresh(),
-      Future.delayed(const Duration(seconds: 1)),
-    ]);
+    await userService.refresh();
     if (!mounted) return;
+    bool isRedirected = await handleInitialLink(initialLink, context: context);
+    if (isRedirected) return;
     Navigator.of(context).pushAndRemoveUntil(
         MaterialPageRoute(builder: (context) => const DashboardView()),
         (route) => false);
   }
 }
 
-handleInitialLink(Uri? initialLink, {BuildContext? context}) async {
-  if (initialLink == null) return;
+Future<bool> handleInitialLink(Uri? initialLink,
+    {BuildContext? context}) async {
+  // initialLink = Uri(path: "note/638b4d8935ed7f33a041b4e3"); // this is left for testing purposes since testing deep links is not trivial
+
+  if (initialLink == null) return false;
   if (initialLink.path.contains('confirmation/')) {
     // user clicked the link in the confirmation email
     List<String> parts = initialLink.path.split('confirmation/');
-    if (parts.length < 2) return;
+    if (parts.length < 2) return false;
     String token = parts[1];
     bool isSucessful = await userService.confirmMail(code: token);
-    if (!isSucessful) return;
+    if (!isSucessful) return false;
     if (context != null) {
       await Future.any([
         Future.delayed(const Duration(seconds: 5)),
@@ -97,6 +101,24 @@ handleInitialLink(Uri? initialLink, {BuildContext? context}) async {
       Navigator.of(context).pushAndRemoveUntil(
           MaterialPageRoute(builder: (context) => introView()),
           (route) => false);
+      return true;
     }
+  } else if (initialLink.path.contains('note/')) {
+    List<String> parts = initialLink.path.split('note/');
+    if (parts.length < 2) return false;
+    String noteId = parts[1];
+
+    Note? note = await noteService.getNote(noteId: noteId);
+    if (note == null || context == null) return false;
+
+    // ignore: use_build_context_synchronously
+    Navigator.of(context).pushAndRemoveUntil(
+        MaterialPageRoute(builder: (context) => const DashboardView()),
+        (route) => false);
+    // ignore: use_build_context_synchronously
+    PersistentNavBarNavigator.pushNewScreen(context,
+        screen: noteView(note: note), withNavBar: false);
+    return true;
   }
+  return false;
 }
