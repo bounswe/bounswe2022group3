fed0dbe7349656d26013045008969f5b7204708d
Salim Kemal Tirit <64011660+salimtirit@users.noreply.github.com>
2022-12-25 20:58:25 +0300

    edit button changed to be inside the floating buttons

--- app/mobile/lib/view/resource/edit_button.dart
+++ app/mobile/lib/view/resource/edit_button.dart
@@ -1,32 +1,13 @@
 import 'package:bucademy/classes/resource/resource.dart';
 import 'package:bucademy/classes/topic/topic.dart';
-import 'package:bucademy/resources/custom_colors.dart';
 import 'package:bucademy/view/resource/edit_resource_page.dart';
 import 'package:flutter/material.dart';
 import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 
-GestureDetector editResourceButton(TopicDetailed t, Resource r, BuildContext context, ChangeNotifier topicPageView,
+editResourceButton(TopicDetailed t, Resource r, BuildContext context,
+    ChangeNotifier topicPageView,
     {bool isClickable = true}) {
-  return GestureDetector(
-    onTap: () async {
-      if(!isClickable) return;
-      PersistentNavBarNavigator.pushNewScreen(context, screen: editResourceView(t,r,topicPageView)); 
-    },
-    child: Padding(
-      padding: const EdgeInsets.all(10),
-      child: OverflowBox(
-          alignment: Alignment.bottomRight,
-          child: Container(
-            padding: const EdgeInsets.all(10),
-            decoration: BoxDecoration(
-                color: CustomColors.main,
-                borderRadius: BorderRadiusDirectional.circular(10),
-              ),
-            child: const Icon(
-              Icons.edit_outlined,
-              color: Colors.white,
-            ),
-          )),
-    ),
-  );
+  if (!isClickable) return;
+  PersistentNavBarNavigator.pushNewScreen(context,
+      screen: editResourceView(t, r, topicPageView));
 }
