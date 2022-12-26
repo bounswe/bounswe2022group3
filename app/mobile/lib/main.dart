a1ebe567cdb7ab4616bc32eaa946dd770e451718
bilozof <bilal.aytekin@boun.edu.tr>
2022-12-26 21:10:25 +0300

    reset login

--- app/mobile/lib/main.dart
+++ app/mobile/lib/main.dart
@@ -21,7 +21,7 @@ void main() {
   navigatorService.controller = PersistentTabController(initialIndex: 0);
   WidgetsFlutterBinding.ensureInitialized();
   //persistenceService.clear();
-  userService.login(email: 'enmuhammet23@gmail.com', password: "Password123*");
+  //userService.login(email: 'enmuhammet23@gmail.com', password: "Password123*");
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then(
     (_) => runApp(const MyApp()),
   );
