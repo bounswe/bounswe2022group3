1f704f5280697fe10754216928cc697d0a2f9834
Furkan Akkurt <71407287+furkanakkurt1335@users.noreply.github.com>
2022-12-27 16:45:49 +0300

    add unit test for topic model

---
+++ app/server/src/tests/topic/topic.test.js
@@ -0,0 +1,27 @@
+const supertest = require("supertest");
+const { dbConnect, dbDisconnect } = require("../utils/db");
+
+let spy;
+let app;
+
+describe("Topic", () => {
+    beforeAll(async () => {
+        dbConnect();
+        const AuthService = require("../../services/auth");
+        spy = jest.spyOn(AuthService, "authorization");
+        app = require("../../app");
+    });
+    afterAll(async () => dbDisconnect());
+    describe("create route", () => {
+        describe("given no space_id was provided", () => {
+            it("should return a 400", async () => {
+                await supertest(app)
+                    .post("/topic")
+                    .send({
+                        "name": "Scalar pitch"
+                    })
+                    .expect(400);
+            });
+        });
+    });
+});
