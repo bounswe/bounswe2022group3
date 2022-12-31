b0e28e5661e17e8c9ce350c696427d078cdd03b5
Salim Kemal Tirit <64011660+salimtirit@users.noreply.github.com>
2022-12-31 22:43:25 +0300

    cahnged the position of listview and refresh indicator to trigger refresh using the whole page

--- app/mobile/lib/view/home/homepage.dart
+++ app/mobile/lib/view/home/homepage.dart
@@ -18,11 +18,112 @@ Widget homepageView() => ViewModelBuilder<HomeViewModel>.reactive(
                   ? "Hello ${userService.user?.name}!"
                   : null,
               context: context),
-          body: RefreshIndicator(
-            onRefresh: (() => viewModel.update()),
-            child: Stack(
-              children: [
-                ListView(
+          body: Stack(
+            children: [
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 8) +
+                        const EdgeInsets.only(bottom: 12),
+                    decoration: const BoxDecoration(
+                        color: CustomColors.main,
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(Constants.borderRadius),
+                          bottomRight:
+                              Radius.circular(Constants.borderRadius),
+                        )),
+                    child: searchBar(
+                        viewModel.search, viewModel.searchBarController,
+                        close: () {
+                      viewModel.closeSearch();
+                      FocusScope.of(context).requestFocus(FocusNode());
+                    }),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 12.0, top: 10),
+                      child: viewModel.isSearchScreen
+                          ? viewModel.isLoading
+                              ? const Center(
+                                  child: CircularProgressIndicator())
+                              : SingleChildScrollView(
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      ...viewModel.searchResults.map(
+                                          (Course c) =>
+                                              searchCourseTile(c, context)),
+                                    ],
+                                  ),
+                                )
+                          : SingleChildScrollView(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const Text(
+                                    'My Spaces',
+                                    style: TextStyles.subtitle,
+                                  ),
+                                  const SizedBox(height: 12),
+                                  viewModel.isLoading
+                                      ? loadingIndicator()
+                                      : SingleChildScrollView(
+                                          scrollDirection: Axis.horizontal,
+                                          child: Row(children: [
+                                            ...viewModel.enrolledCourses.map(
+                                                (Course c) =>
+                                                    courseTile(c, context))
+                                          ]),
+                                        ),
+                                  const SizedBox(height: 20),
+                                  const Text(
+                                    'Recommended Spaces',
+                                    style: TextStyles.subtitle,
+                                  ),
+                                  const SizedBox(height: 20),
+                                  viewModel.isLoading
+                                      ? loadingIndicator()
+                                      : SingleChildScrollView(
+                                          scrollDirection: Axis.horizontal,
+                                          child: Row(
+                                            children: [
+                                              ...viewModel.recommendedSpaces
+                                                  .map((Course c) =>
+                                                      courseTile(c, context))
+                                            ],
+                                          ),
+                                        ),
+                                  const SizedBox(height: 20),
+                                  const Text(
+                                    'Discover Top Spaces',
+                                    style: TextStyles.subtitle,
+                                  ),
+                                  const SizedBox(height: 12),
+                                  viewModel.isLoading
+                                      ? loadingIndicator()
+                                      : SingleChildScrollView(
+                                          scrollDirection: Axis.horizontal,
+                                          child: Row(
+                                            children: [
+                                              ...viewModel.courses.map(
+                                                  (Course c) =>
+                                                      courseTile(c, context))
+                                            ],
+                                          ),
+                                        ),
+                                ],
+                              ),
+                            ),
+                    ),
+                  ),
+                ],
+              ),
+              RefreshIndicator(
+                onRefresh: () => viewModel.update(),
+                child: ListView(
                   // this listview is needed to trigger refresh
                   padding: EdgeInsets.zero,
                   shrinkWrap: true,
@@ -32,109 +133,8 @@ Widget homepageView() => ViewModelBuilder<HomeViewModel>.reactive(
                     )
                   ],
                 ),
-                Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.symmetric(
-                              horizontal: 20, vertical: 8) +
-                          const EdgeInsets.only(bottom: 12),
-                      decoration: const BoxDecoration(
-                          color: CustomColors.main,
-                          borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(Constants.borderRadius),
-                            bottomRight:
-                                Radius.circular(Constants.borderRadius),
-                          )),
-                      child: searchBar(
-                          viewModel.search, viewModel.searchBarController,
-                          close: () {
-                        viewModel.closeSearch();
-                        FocusScope.of(context).requestFocus(FocusNode());
-                      }),
-                    ),
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 12.0, top: 10),
-                        child: viewModel.isSearchScreen
-                            ? viewModel.isLoading
-                                ? const Center(
-                                    child: CircularProgressIndicator())
-                                : SingleChildScrollView(
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        ...viewModel.searchResults.map(
-                                            (Course c) =>
-                                                searchCourseTile(c, context)),
-                                      ],
-                                    ),
-                                  )
-                            : SingleChildScrollView(
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    const Text(
-                                      'My Spaces',
-                                      style: TextStyles.subtitle,
-                                    ),
-                                    const SizedBox(height: 12),
-                                    viewModel.isLoading
-                                        ? loadingIndicator()
-                                        : SingleChildScrollView(
-                                            scrollDirection: Axis.horizontal,
-                                            child: Row(children: [
-                                              ...viewModel.enrolledCourses.map(
-                                                  (Course c) =>
-                                                      courseTile(c, context))
-                                            ]),
-                                          ),
-                                    const SizedBox(height: 20),
-                                    const Text(
-                                      'Recommended Spaces',
-                                      style: TextStyles.subtitle,
-                                    ),
-                                    const SizedBox(height: 20),
-                                    viewModel.isLoading
-                                        ? loadingIndicator()
-                                        : SingleChildScrollView(
-                                            scrollDirection: Axis.horizontal,
-                                            child: Row(
-                                              children: [
-                                                ...viewModel.recommendedSpaces
-                                                    .map((Course c) =>
-                                                        courseTile(c, context))
-                                              ],
-                                            ),
-                                          ),
-                                    const SizedBox(height: 20),
-                                    const Text(
-                                      'Discover Top Spaces',
-                                      style: TextStyles.subtitle,
-                                    ),
-                                    const SizedBox(height: 12),
-                                    viewModel.isLoading
-                                        ? loadingIndicator()
-                                        : SingleChildScrollView(
-                                            scrollDirection: Axis.horizontal,
-                                            child: Row(
-                                              children: [
-                                                ...viewModel.courses.map(
-                                                    (Course c) =>
-                                                        courseTile(c, context))
-                                              ],
-                                            ),
-                                          ),
-                                  ],
-                                ),
-                              ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
+              ),
+            ],
           )),
     );
 
